1ad559bcccc1dd0fc3ecdd820589a09c99858764
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-11-28 12:32:50 -0300

    Adição de novos tipos

--- rick-and-morty-web-app/src/intefaces/characters.ts
+++ rick-and-morty-web-app/src/intefaces/characters.ts
@@ -2,4 +2,15 @@ export interface ICharacter {
   id: number | string;
   name: string;
   image: string;
+  species: string;
+  location: ILocation;
+  episode: IEpisede
+}
+
+export interface ILocation {
+  name: string;
+}
+
+export interface IEpisede {
+  name: string;
 }
