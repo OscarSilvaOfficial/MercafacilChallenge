f8bc8604f515a885ef66d70c18129abf92e528c7
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-11-28 12:33:45 -0300

    Melhoria no display

--- rick-and-morty-web-app/src/views/characters/Characters.vue
+++ rick-and-morty-web-app/src/views/characters/Characters.vue
@@ -2,23 +2,27 @@
   <div class="list">
     <h2 class='logo font-rem d-flex justify-center align-center' style='color: red; font-size: 6rem'>R&MFlix</h2>
     <vueper-slides
-      v-if="showEpisedes"
+      ref="vueperslides1"
+      v-if="showCharacters"
       slide-multiple
       class="no-shadow mt-11"
       :visible-slides="6"
       :bullets="false"
       :arrows="false"
-      :slide-ratio="1 / 6"
-      :gap="5"
+      :slide-ratio="2 / 14"
       :dragging-distance="70">
       <vueper-slide 
-        :id="'character-' + character.name"
-        :style="getBackground(character.image)" 
+        :style="getStyles(character.image)" 
         class="bg-screen" 
         v-for="(character, i) in characters" 
         :key="i"
-      />
+      >
+        <template #content>
+          <div @mouseover="setCharacterDetails(character)" @mouseleave="closeCharacterDetails()" style="width: 100%; height: 100%"></div>
+        </template>
+      </vueper-slide>
     </vueper-slides>
+    <Card :character="selectedCharacter" v-if="showCharacterDetails" />
   </div>
 </template>
 
@@ -30,32 +34,44 @@ import { Component, Vue } from 'vue-property-decorator';
 import { GraphQLService } from '@/services/graphql'
 import { ICharacter } from '@/intefaces/characters'
 import { CHARACTERS } from './queries'
-import 'vueperslides/dist/vueperslides.css'
+import Card from '@/views/characters/Card.vue'
 
 @Component({
-  components: { VueperSlides, VueperSlide },
+  components: { VueperSlides, VueperSlide, Card },
 })
 export default class Characters extends Vue {
 
   graphql = new GraphQLService()
   characters?: ICharacter[]
-  showEpisedes = false
+  selectedCharacter?: ICharacter | null
+  showCharacters = false
+  showCharacterDetails = false
 
   async created(): Promise<void> {
     const response: any = await this.graphql.get(CHARACTERS)
     this.characters = await response.data.data.characters.results
-    this.showEpisedes = true
+    this.showCharacters = true
   }
 
-  getBackground(image: string): string {
-    return `background-image: url(${image}); background-size: cover`
+  setCharacterDetails(character: ICharacter): void {
+    console.log(character)
+    this.selectedCharacter = character
+    this.showCharacterDetails = true
+  }
+
+  closeCharacterDetails(): void {
+    this.showCharacterDetails = false
+  }
+
+  getStyles(image: string): string {
+    return `background-image: url(${image}); background-size: contain;`
   }
 }
 </script>
 
 <style lang="scss">
 .logo {
-  margin-top: 2vh;
+  margin-top: 0;
 }
 .bg-screen {
   transition: all .5s;
