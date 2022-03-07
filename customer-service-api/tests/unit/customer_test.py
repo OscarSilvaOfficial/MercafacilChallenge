84728a09c694e153be096c988cfcf13cd6166523
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-03-06 23:26:38 -0300

    Add teste com telefone inválido

--- customer-service-api/tests/unit/customer_test.py
+++ customer-service-api/tests/unit/customer_test.py
@@ -1,10 +1,11 @@
 import phonenumbers
 from api.core.customer import Customer
+from api.core.validators.phone_number_validator import InvalidPhoneNumberException
 import logging
 
 LOGGER = logging.getLogger(__name__)
 
-def test_macapa_customer():
+def test_valid_macapa_customer():
   customer = Customer(
     email="cliente@gmail.com.br",
     customer_name="macapa",
@@ -20,7 +21,7 @@ def test_macapa_customer():
     assert contact['name'].isupper()
     assert phonenumbers.is_valid_number(phonenumbers.parse(contact['phone'], 'BR'))
     
-def test_varejao_customer():
+def test_valid_varejao_customer():
   customer = Customer(
     email="cliente@gmail.com.br",
     customer_name="varejao",
@@ -34,6 +35,34 @@ def test_varejao_customer():
   for contact in customer.contacts:
     LOGGER.info(contact)
     assert phonenumbers.is_valid_number(phonenumbers.parse(contact['phone'], 'BR'))
+
+def test_invalid_macapa_customer():
+  try:
+    Customer(
+      email="cliente@gmail.com.br",
+      customer_name="macapa",
+      contacts=[
+        {"phone": "+55119999999991", "name": "Geralt de Rivia"},
+        {"phone": "479346329341", "name": "Ermanoteu"},
+        {"phone": "+55 (47) 93463-92931", "name": "Mikalateia"},
+      ]
+    )
+  except Exception as e:
+    assert type(e) is InvalidPhoneNumberException
+    
+def test_invalid_varejao_customer():
+  try:
+    Customer(
+      email="cliente@gmail.com.br",
+      customer_name="varejao",
+      contacts=[
+        {"phone": "+55119999999991", "name": "Geralt de Rivia"},
+        {"phone": "479346329341", "name": "Ermanoteu"},
+        {"phone": "+55 (47) 93463-92931", "name": "Mikalateia"},
+      ]
+    )
+  except Exception as e:
+    assert type(e) is InvalidPhoneNumberException
 
   
   
